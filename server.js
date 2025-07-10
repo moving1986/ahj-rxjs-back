7434f5c4c3edf762ba9e1b91c42c607abb8585e6
Aleksei Mezentsev <aleksei_mezencev@mail.ru>
2025-07-10 09:43:06 +0700

    fix cors again

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-
+app.use(cors());
 
 // Генерация случайных сообщений
 function generateMessages(count = 1) {
