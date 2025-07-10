ccb98ffdc7464107009fe7c2bb5eed3e73b154c5
Aleksei Mezentsev <aleksei_mezencev@mail.ru>
2025-07-10 09:39:33 +0700

    fix cors

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const port = 3000;
+const cors = require('cors');
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 // Генерация случайных сообщений
 function generateMessages(count = 1) {
